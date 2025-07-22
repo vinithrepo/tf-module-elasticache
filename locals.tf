14daae705b68d1277cb6b05005550d8c196adf5a
Vinith Amma <vinithamma007@gmail.com>
2025-07-22 21:42:25 +0530

    app module alb&tg s-42

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   name_prefix = "${var.env}-${var.elasticache_type}-elasticache"
 
-  tags = merge(var.tags, {tf-module-name = "elasticache"}, {env = var.env} )
+  tags = merge(var.tags, { tf-module-name = "elasticache" }, { env = var.env } )
 }
--- main.tf
+++ main.tf
@@ -37,10 +37,10 @@ resource "aws_elasticache_cluster" "main" {
   parameter_group_name = aws_elasticache_parameter_group.main.name
   engine_version       = var.engine_version
   port                 = var.port
-  subnet_group_name = aws_elasticache_subnet_group.main.name
-  security_group_ids = [aws_security_group.main.id]
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
+  security_group_ids   = [aws_security_group.main.id]
 
-  tags        = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
+  tags = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
 }
 
 
